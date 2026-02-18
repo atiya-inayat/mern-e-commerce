502918c1d6c6d786a55ee5db4a529db11e87ab96
atiya-inayat <atiyainayat869@gmail.com>
2026-02-18 21:24:33 +0500

    creating schema for order

--- backend/src/modules/order/order.model.ts
+++ backend/src/modules/order/order.model.ts
@@ -1,6 +1,4 @@
-// We need to track the shipping address, the items, the payment result, and the price breakdown.
-
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IOrderItem {
   name: string;
@@ -26,7 +24,6 @@ export interface IOrder extends Document {
     update_time: string;
     email_address: string;
   };
-
   itemsPrice: number;
   taxPrice: number;
   shippingPrice: number;
@@ -36,3 +33,109 @@ export interface IOrder extends Document {
   isDelivered: boolean;
   deliveredAt: Date;
 }
+
+const orderSchema = new Schema<IOrder>(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        qty: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+    shippingAddress: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: {
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+      update_time: {
+        type: String,
+      },
+      email_address: {
+        type: String,
+      },
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Order = mongoose.model<IOrder>("Order", orderSchema);
